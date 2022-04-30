f2d7081a1df5fcf9a3824131c8a63fcdc407af35
Pranchipandey <pranchi.pandey_cs19@gla.ac.in>
2022-04-30 14:30:26 +0530

    .

--- Recursion/sum.java
+++ Recursion/sum.java
@@ -1,3 +1,16 @@
-public class sum {
+public class Sum {
+     static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
 
+    public static void main(String[] args) {
+        int ans = sum(3);
+        System.out.println(ans);
+    }
 }
